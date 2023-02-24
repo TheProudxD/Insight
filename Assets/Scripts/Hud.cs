c75d69f9e4cbd9770ea309ef34bfaf8efc51f2e0
proud <84202885+TheProudxD@users.noreply.github.com>
2023-02-24 21:48:05 +0300

    Game starts with menu

--- Assets/Scripts/Hud.cs
+++ Assets/Scripts/Hud.cs
@@ -1,14 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Hud : MonoBehaviour
 {
-    [SerializeField] private GameObject _settingsWindow, _pauseMenu;
-    private bool _isFullScreen, _isPause;
+    [SerializeField] private GameObject _settingsWindow, _pauseMenu, _menuUI;
+    private bool _isFullScreen, _isPause, _isInMenu;
 
     public static Hud Instance;
     private void Awake()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+            return;
         if (Instance != null)
         {
             Destroy(gameObject);
@@ -17,6 +20,7 @@ public class Hud : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            _isInMenu=true;
         }
     }
     private void Update()
@@ -28,13 +32,13 @@ public class Hud : MonoBehaviour
     {
         _isPause = true;
         Time.timeScale = 0;
-        _pauseMenu.SetActive(_isPause);
+        if (!_pauseMenu.IsUnityNull()) _pauseMenu.SetActive(_isPause);
     }
     private void DisablePauseMode()
     {
         _isPause = false;
         Time.timeScale = 1;
-        _pauseMenu.SetActive(_isPause);
+        if (!_pauseMenu.IsUnityNull()) _pauseMenu.SetActive(_isPause);
     }
     private void SetToPauseMode()
     {
@@ -54,18 +58,26 @@ public class Hud : MonoBehaviour
     }
     public void OpenSettingsWindow()
     {
-        _pauseMenu.SetActive(false);
+        _isInMenu = false;
+        if (!_menuUI.IsUnityNull()) _menuUI.SetActive(false);
+        if (!_pauseMenu.IsUnityNull()) _pauseMenu.SetActive(false);
         _settingsWindow.SetActive(true);
     }
     public void CloseSettingsWindow()
     {
-        if (_isPause) _pauseMenu.SetActive(true);
+        if (_isPause && !_pauseMenu.IsUnityNull()) _pauseMenu.SetActive(true);
         _settingsWindow.SetActive(false);
     }
     public void OpenMenu()
     {
+        _isInMenu = true;
+        if (!_menuUI.IsUnityNull()) _menuUI.SetActive(false);
         DisablePauseMode();
         SceneManager.LoadScene(0);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
     public void Exit()
     {
